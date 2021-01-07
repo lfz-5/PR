a591aa98f3444fc2d65bd791940452e615bf399b
lfz_5 <lifazeng818@163.com>
2021-01-07 19:41:36 +0800

    p5

--- train.py
+++ train.py
@@ -9,8 +9,8 @@ from tqdm import tqdm
 from VGG_16_model import VGG_16
 
 #定义学习速率
-BATCH_SIZE = 256
-LEARNING_RATE = 0.005
+BATCH_SIZE = 32
+LEARNING_RATE = 0.0001
 EPOCHES = 10
 
 # 下载训练集 CIFAR-10 10分类训练集
